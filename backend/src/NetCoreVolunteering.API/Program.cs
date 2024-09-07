d0440dc897e7be534e96c309dd52ecacb116b850
Vanya Chernov <vanyachernov2001@gmail.com>
2024-09-07 13:54:09 +0300

    Add Serilog. Set up Seq && docker-compose

--- backend/src/NetCoreVolunteering.API/Program.cs
+++ backend/src/NetCoreVolunteering.API/Program.cs
@@ -1,14 +1,28 @@
 using NetCoreVolunteering.API.Validation;
 using NetCoreVolunteering.Application;
 using NetCoreVolunteering.Infrastructure;
+using Serilog;
+using Serilog.Events;
 using SharpGrip.FluentValidation.AutoValidation.Mvc.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 {
+    Log.Logger = new LoggerConfiguration()
+        .WriteTo.Console()
+        .WriteTo.Debug()
+        .WriteTo.Seq(builder.Configuration.GetConnectionString("Seq") 
+                     ?? throw new ArgumentNullException())
+        .MinimumLevel.Override("Microsoft.AspNetCore.Hosting", LogEventLevel.Warning)
+        .MinimumLevel.Override("Microsoft.AspNetCore.Mvc", LogEventLevel.Warning)
+        .MinimumLevel.Override("Microsoft.AspNetCore.Routing", LogEventLevel.Warning)
+        .CreateLogger();
+    
     builder.Services.AddControllers();
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
 
+    builder.Services.AddSerilog();
+    
     builder.Services
         .AddInfrastructure()
         .AddApplication();
@@ -26,7 +40,8 @@ var app = builder.Build();
         app.UseSwagger();
         app.UseSwaggerUI();
     }
-
+    
+    app.UseSerilogRequestLogging();
     app.UseHttpsRedirection();
     app.UseAuthorization();
     app.MapControllers();
